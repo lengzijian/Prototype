d81e52c84e21358d82dabd64d055bb63affa7639
liye <378712097@qq.com>
2012-10-25 14:41:19 +0800

    add code

---
+++ Prototype.h
@@ -0,0 +1,32 @@
+//Prototype.h 
+
+
+#ifndef _PROTOTYPE_H_ 
+#define _PROTOTYPE_H_ 
+
+class Prototype 
+{ 
+public: 
+    virtual ~Prototype(); 
+    virtual Prototype* Clone() const = 0; 
+protected: 
+    Prototype(); 
+private: 
+
+}; 
+
+
+class ConcretePrototype:public Prototype  
+{
+public: 
+    ConcretePrototype(); 
+    ConcretePrototype(const ConcretePrototype& cp); 
+    ~ConcretePrototype(); 
+    Prototype *Clone() const; 
+protected: 
+
+private: 
+
+}; 
+
+#endif
